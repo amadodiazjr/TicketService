7370d4e1c1fba647e4c6dc2bcd897923cbc0ec12
amado <amado.diaz.jr@gmail.com>
2016-05-22 17:53:22 -0400

    updated test name

--- src/test/java/integration/WalmartTicketServiceTest.java
+++ src/test/java/integration/WalmartTicketServiceTest.java
@@ -5,7 +5,7 @@ import org.junit.Test;
 public class WalmartTicketServiceTest {
 	
 	@Test
-	public void numSeatsAvailableShallReturnTotalNumberOfAllVenuesWhenLevelIsNotProvided() {
+	public void numSeatsAvailableShallReturnTotalNumberOfAllLevelsWhenLevelIsNotProvided() {
 		
 	}
 	
